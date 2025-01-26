aa347ceaf2487ea57657ac1a13922da1859910de
Santosh <sahosantosh117@gmail.com>
2025-01-26 11:10:45 +0530

    new chnges

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { connectDB } = require("./config/database");
 const User=require("./models/user")
 const app = express();
+const validator = require('validator');
 
 app.use(express.json());
 //express.json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object. This method is called as a middleware in your application using the code: app.use(express.json());
@@ -19,14 +20,102 @@ app.post("/signup",async(req,res)=>{
   //   gender:"male"
   // }
   const user = new User(req.body);
+  
 
   try {
+const isEmailValid=validator.isEmail(user.emailId);
+const isPasswordValid=validator.isStrongPassword(user.password);
+const isphotoUrl=validator.isURL(user.photoUrl);
+
+if(!isEmailValid){
+  throw new Error("Email is invalid");
+}
+if (!isPasswordValid) {
+  throw new Error("Email is invalid");
+}
+if (!isphotoUrl) {
+  throw new Error("Email is invalid");
+}
+
+
+
     await user.save();
     res.send("User added sucessfully");
   } catch (error) {
+    console.error("Error saving user:", error);
     res.status(400).send("User not added");
   }
 })
+
+
+app.get("/user",async(req,res)=>{
+  //const userEmail=req.body.emailId;
+  const userId=req.body._id;
+try{
+    //const user= await User.findOne({ emailId: userEmail });
+    const user=await User.findById(userId);
+    res.send(user);
+  //   if(users.length===0){
+  //  res.status(400).send("User not found");
+  //   }else{
+  //     res.send(users);
+  //   }
+  
+}
+catch(error){
+  res.status(400).send("Something went wrong");
+}
+})
+
+
+
+//Feed API-GET /feed -get all the users from database
+app.get("/feed",async(req,res)=>{
+  try{
+const users=await User.find({});
+res.send(users);
+  }
+  catch(error){
+    res.status(400).send("Something went wrong");
+  }
+})
+
+app.delete("/user",async(req,res)=>{
+  const userId=req.body.userId;
+  try{
+const user=await User.findByIdAndDelete(userId);
+res.send("User deleted sucessfully");
+  }catch(error){
+    res.status(400).send("Something went wrong");
+  }
+})
+
+
+//update the data
+app.patch("/user",async(req,res)=>{
+  const userId=req.params.userId;
+  const updateData=req.body;
+
+  try{
+    const ALLOWED_UDATES=["userId","photourl","about","skills"];
+    const isUpdatedAllowed=Object.keys(upadateData).every((update)=>ALLOWED_UDATES.includes(update));
+    if(!isUpdatedAllowed){
+      throw new Error("Invalid updates");
+    }
+    if(updateData.skills.length>10){
+      throw new Error("Skills cannot be more than 10");
+    }
+const user = await User.findByIdAndUpdate({ _id: userId }, updateData, {
+  options: "before",
+  runvalidators: true,
+});
+console.log(user);
+res.send("User updated sucessfully");
+  }catch(error){
+    res.status(400).send("Something went wrong");
+  }
+})
+
 
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,55 @@
 const mongoose=require("mongoose")
+const validator = require('validator'); 
 
 const userSchema=new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+    minlenght:5,
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        lowercase:true,
+        required:true,
+        unique:true,
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is invalid");
+            }
+        }
     },
     password:{
         type:String,
+        required:true,
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
+        
     },
     gender:{
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("gender data is invalid");
+        }
+    },
+    },
+    photoUrl:{
         type:String
-    }
-});
+    },
+    about:{
+        type:String,
+        default:"This is default value"  
+    },
+    skills:{
+        type:[String]
+    },
+},{timestamps:true});
 
 module.exports=mongoose.model("User",userSchema)
 

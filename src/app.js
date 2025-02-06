dc9378596c156dc5fafc13a5c38bb5f134742cf3
Santosh <sahosantosh117@gmail.com>
2025-02-06 16:51:27 +0530

    connect frontend

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
+const cors = require("cors");
 
 const app = express();
 const cookieParser = require("cookie-parser");
 
+app.use(cors(
+  {
+    origin:"http://localhost:5173",
+    credentials:true
+  }
+))
 app.use(express.json());
 //express.json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object. This method is called as a middleware in your application using the code: app.use(express.json());
 app.use(cookieParser());
